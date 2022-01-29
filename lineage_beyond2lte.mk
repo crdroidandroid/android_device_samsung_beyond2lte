97959b5b4cba7f082470945ca415f20d3973bd8a
Orel <Orely4242@gmail.com>
2022-01-29 20:44:52 +0200

    beyond2lte: adapt to crDroid

--- lineage_beyond2lte.mk
+++ lineage_beyond2lte.mk
@@ -17,32 +17,29 @@
 ## Inherit from generic products, most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-
-## Product API level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
-## Inscreen Fingerprint HAL
-TARGET_HAVE_FOD := true
+## Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
 ## Inherit from beyond2lte device
 $(call inherit-product, device/samsung/beyond2lte/device.mk)
+
+## Inherit some common crDroid stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 ## Boot Animation
 TARGET_BOOTANIMATION_HALF_RES := true
 TARGET_SCREEN_HEIGHT := 3040
 TARGET_SCREEN_WIDTH := 1440
-
-## Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-## Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+TARGET_HAVE_FOD := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier, this must come after all inclusions
 PRODUCT_NAME := lineage_beyond2lte
 PRODUCT_DEVICE := beyond2lte
-PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-G975F
-PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := Samsung
+PRODUCT_MODEL := Galaxy S10+
+PRODUCT_MANUFACTURER := Samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung
